f7ed412da3e713fd29ef9efad980f49f4fd0e061
Alexey <azarenkov20141@icloud.com>
2025-04-05 20:37:33 +0500

    refactoring ApiError

--- src/controllers/shared/errors.rs
+++ src/controllers/shared/errors.rs
@@ -1,16 +1,34 @@
-use actix_web::{HttpResponse, ResponseError};
+use actix_web::{http::StatusCode, HttpResponse, ResponseError};
+use serde::Serialize;
 
 use crate::models::errors::ServiceError;
 
+#[derive(Serialize)]
+struct ApiError {
+    message: String,
+    status: u16,
+}
+
+impl ApiError {
+    fn new(message: String, status: u16) -> Self {
+        Self { message, status }
+    }
+}
+
 impl ResponseError for ServiceError {
     fn error_response(&self) -> HttpResponse<actix_web::body::BoxBody> {
+        let response = ApiError::new(self.to_string(), self.status_code().as_u16());
+        HttpResponse::build(self.status_code()).json(response)
+    }
+
+    fn status_code(&self) -> StatusCode {
         match self {
-            ServiceError::UserAlreadyExists(e) => HttpResponse::Accepted().json(e),
-            ServiceError::InvalidToken(e) => HttpResponse::BadRequest().json(e),
-            ServiceError::DataNotFound(e) => HttpResponse::NotFound().json(e),
-            ServiceError::InternalServerError(e) => HttpResponse::InternalServerError().json(e),
-            ServiceError::ReqwestError(e) => HttpResponse::NotFound().json(e),
-            ServiceError::DeadlineSortingError(e) => HttpResponse::NotFound().json(e.to_string()),
+            ServiceError::UserAlreadyExists(_) => StatusCode::ACCEPTED,
+            ServiceError::InvalidToken(_) => StatusCode::BAD_REQUEST,
+            ServiceError::DataNotFound(_) => StatusCode::NOT_FOUND,
+            ServiceError::InternalServerError(_) => StatusCode::INTERNAL_SERVER_ERROR,
+            ServiceError::ReqwestError(_) => StatusCode::NOT_FOUND,
+            ServiceError::DeadlineSortingError(_) => StatusCode::NOT_FOUND,
         }
     }
 }
